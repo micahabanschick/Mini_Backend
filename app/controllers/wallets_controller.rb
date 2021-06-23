0d4cf92a1b21deb44e1843845af5da6f011e6781
micahabanschick <micha.ban@gmail.com>
2021-06-22 23:21:27 -0400

    rails g controller products/projects/wallets

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -1,2 +1,22 @@
 class WalletsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def index
+  end
+
+  def show
+  end
 end
